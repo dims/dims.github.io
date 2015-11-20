6178ad155ba720aa0fa29426f9feaae384c4fa91
Davanum Srinivas <davanum@gmail.com>
2015-11-20 16:49:02 -0500

    Add shell script for travis

---
+++ oslo-from-master.sh
@@ -0,0 +1,40 @@
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.cache.git#egg=oslo.cache
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.concurrency.git#egg=oslo.concurrency
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.config.git#egg=oslo.config
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.context.git#egg=oslo.context
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.i18n.git#egg=oslo.i18n
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.log.git#egg=oslo.log
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.messaging.git#egg=oslo.messaging
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.middleware.git#egg=oslo.middleware
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.policy.git#egg=oslo.policy
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.privsep.git#egg=oslo.privsep
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.reports.git#egg=oslo.reports
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.rootwrap.git#egg=oslo.rootwrap
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.serialization.git#egg=oslo.serialization
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.service.git#egg=oslo.service
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.utils.git#egg=oslo.utils
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.versionedobjects.git#egg=oslo.versionedobjects
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.vmware.git#egg=oslo.vmware
+pip install -v -U \
+    -e git+https://github.com/openstack/oslosphinx.git#egg=oslosphinx
+pip install -v -U \
+    -e git+https://github.com/openstack/oslotest.git#egg=oslotest
+pip install -v -U \
+    -e git+https://github.com/openstack/oslo.db.git#egg=oslo.db
