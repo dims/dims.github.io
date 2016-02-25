9dabd0781aa975bf2e153f2d24cc86ec6359436f
Davanum Srinivas <davanum@gmail.com>
2016-02-25 11:51:04 -0800

    update script

--- rabbit-stuff-from-master.sh
+++ rabbit-stuff-from-master.sh
@@ -1,5 +1,9 @@
-pip install -q -U \
-    -e git+https://github.com/dims/py-amqp.git#egg=py-amqp
-pip install -q -U \
+#!/bin/bash
+set -x
+pip uninstall kombu -y
+pip uninstall amqp -y
+pip install -q -U --force \
     -e git+https://github.com/dims/kombu.git#egg=kombu
+pip install -q -U --force \
+    -e git+https://github.com/dims/py-amqp.git#egg=amqp
 pip freeze | grep -e kombu -e amqp
